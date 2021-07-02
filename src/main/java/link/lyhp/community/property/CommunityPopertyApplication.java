0a9cc9a7d5764b6908351ceeebac842088f14dfa
yaojie <yaojie@unwulian.com>
2021-07-02 15:05:40 +0800

    Initial commit

---
+++ src/main/java/link/lyhp/community/property/CommunityPopertyApplication.java
@@ -0,0 +1,13 @@
+package link.lyhp.community.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPopertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityPopertyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/lyhp/community/property/CommunityPopertyApplicationTests.java
@@ -0,0 +1,13 @@
+package link.lyhp.community.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPopertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
